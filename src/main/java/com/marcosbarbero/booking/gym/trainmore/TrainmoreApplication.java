c0078aa72ef33e87beb70b556d9f4ac10bf1d201
Marcos Barbero <marcos.hgb@gmail.com>
2020-06-27 21:00:00 +0200

    Initial commit

---
+++ src/main/java/com/marcosbarbero/booking/gym/trainmore/TrainmoreApplication.java
@@ -0,0 +1,13 @@
+package com.marcosbarbero.booking.gym.trainmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainmoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainmoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosbarbero/booking/gym/trainmore/TrainmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosbarbero.booking.gym.trainmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainmoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
